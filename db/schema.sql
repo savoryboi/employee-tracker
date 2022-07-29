1012000c71127dc9ae68b69740f31633fd2946d5
savoryboi <andbjerk@yahoo.com>
2022-07-29 13:28:54 -0500

    dummy db setup

---
+++ db/schema.sql
@@ -0,0 +1,32 @@
+DROP DATABASE IF EXISTS emp_db;
+
+CREATE DATABASE emp_db;
+
+USE emp_db;
+
+DROP TABLE IF EXISTS department;
+DROP TABLE IF EXISTS roles;
+DROP TABLE IF EXISTS employee;
+
+
+CREATE TABLE department(
+    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY, 
+    dep_name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE roles(
+    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    title VARCHAR(30) NOT NULL, 
+    salary DECIMAL (6, 3),
+    department_id INT, 
+    FOREIGN KEY (department_id) REFERENCES department(id)
+);
+
+CREATE TABLE employee(
+    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY, 
+    first_name VARCHAR(30) NOT NULL, 
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    FOREIGN KEY (role_id) REFERENCES roles(id), 
+    manager_id INT DEFAULT NULL
+);
