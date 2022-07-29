ab393d90a89dd7a468105af8eb4deb0a5ebb06d5
savoryboi <andbjerk@yahoo.com>
2022-07-29 18:51:14 -0500

    back to menu function

--- app.js
+++ app.js
@@ -21,22 +21,49 @@ const dep_prompts = [
 
 const role_prompts = [
     {
-        name: 'new_role', 
-        type: 'input', 
+        name: 'new_role',
+        type: 'input',
         message: 'Name of new role:'
-    }, 
+    },
     {
-        name: 'salary', 
-        type: 'number', 
-        message: 'New role salary:'
-    }, 
+        name: 'salary',
+        type: 'number',
+        message: 'New role salary (in thousands):'
+    },
     {
-        name: 'department', 
-        type: 'list', 
-        message: 'Department of new role:', 
-        choices: ['Management', 'Sales', 'Communications', 'HR', 'Engineering']
+        name: 'department',
+        type: 'list',
+        message: 'Department of new role:',
+        choices: ['Sales', 'Communications', 'Management', 'Engineering']
     }
 ];
+
+
+
+const emp_prompts = [
+    {
+        name: 'first_name',
+        type: 'input',
+        message: 'Employee first name:'
+    },
+    {
+        name: 'last_name',
+        type: 'input',
+        message: 'Employee last name:'
+    },
+    {
+        name: 'role',
+        type: 'list',
+        message: 'Employee role:', 
+        choices: ['Manager', 'Supervisor', 'Comm Lead', 'Salesperson']
+    },
+    {
+        name: 'manager',
+        type: 'input',
+        message: 'Manager (if applicable):'
+    },
+]
+
 
 function menu() {
     inquirer.prompt(nav_menu)
@@ -45,13 +72,23 @@ function menu() {
         });
 };
 
+function backToMenu() {
+    inquirer.prompt({
+        name: 'back',
+        message: 'press enter for main menu'
+    }).then(answer => {
+        menu();
+    })
+}
+
 function selectedOption(choice) {
     if (choice.nav === 'View All Employees') {
 
         db.query('SELECT * FROM employee', (err, data) => {
             if (err) return console.log(err);
 
-            return console.table(data);
+            console.table(data);
+            backToMenu();
         });
     };
 
@@ -60,8 +97,9 @@ function selectedOption(choice) {
         db.query('SELECT * FROM department', (err, data) => {
             if (err) return console.table(err);
 
-            return console.table(data);
-        });
+            console.table(data);
+            backToMenu();
+        })
     };
 
     if (choice.nav === 'View All Roles') {
@@ -69,7 +107,8 @@ function selectedOption(choice) {
         db.query('SELECT * FROM roles', (err, data) => {
             if (err) return console.log(err);
 
-            return console.table(data);
+            console.table(data);
+            backToMenu();
 
         });
     };
@@ -89,7 +128,7 @@ function selectedOption(choice) {
 
 function newDepartment() {
 
-      inquirer.prompt(dep_prompts)
+    inquirer.prompt(dep_prompts)
         .then(answers => {
             console.log(`Now adding ${answers.new_dep}`);
             db.query(`INSERT INTO department (dep_name) VALUES ('${answers.new_dep}')`);
@@ -102,25 +141,48 @@ function newRole() {
     let departmentId;
 
     inquirer.prompt(role_prompts)
-      .then(answers => {
-          console.log(`Now adding ${answers.new_role}`);
-          db.query(`INSERT INTO roles (dep_name) VALUES ('${answers.new_role}')`);
-          db.query(`INSERT INTO roles (salary) VALUES ('${answers.salary}')`);
-          
-          if (answers.department === 'Manager') {
-            departmentId = 1;
-          }else if(answers.department === 'Communications') {
-            departmentId = 2;
-          } else if(answers.department === 'Management') {
-            departmentId = 3;
-          } 
+        .then(answers => {
+            console.log(`Now adding ${answers.new_role}`);
 
-          db.query(`INSERT INTO roles (deparment_id) VALUES (${departmentId})`);
+            if (answers.department === 'Sales') {
+                departmentId = 1;
+            } else if (answers.department === 'Communications') {
+                departmentId = 2;
+            } else if (answers.department === 'Management') {
+                departmentId = 3;
+            } else if (answers.department === 'Engineering') {
+                departmentId = 4;
+            };
 
-      }).then(() => {
-          menu();
-      })
+            db.query(`INSERT INTO roles (title, salary, department_id) VALUES ('${answers.new_role}', ${answers.salary}.000, ${departmentId})`)
+        }).then(() => {
+            menu();
+        })
 };
+
+// first_name, last_name, role, and manager
+function newEmployee() {
+
+    inquirer.prompt(emp_prompts)
+        .then(answers => {
+            console.log(`Now Adding ${answers.first_name}`)
+
+            if (answers.role === 'Manager') {
+                roleId = 1;
+            } else if (answers.role === 'Supervisor') {
+                roleId = 2;
+            } else if (answers.role === 'Comm Lead') {
+                roleId = 3;
+            } else if (answers.role === 'Sales'){
+                roleId = 4;
+            };
+
+            db.query(`INSERT INTO employee (first_name, last_name, role_id, manager) VALUES ('${answers.first_name}', '${answers.last_name}', ${roleId}, '${answers.manager}')`);
+        }).then(() => {
+            menu();
+        })
+}
+
 
 
 // .then((answer) => {
--- db/connection.js
+++ db/connection.js
@@ -4,6 +4,7 @@ const db_connection =  {
     host: 'localhost',
     database: 'emp_db',
     user: 'root',
+    multipleStatements: true
 };
 
 const connection = mysql.createPool(db_connection);
