73e1074420591f2f7e64d5d9e7805fc43e84fdaf
savoryboi <andbjerk@yahoo.com>
2022-07-30 14:21:20 -0500

    add role functioning

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const db = require('./db/connection');
 const inquirer = require('inquirer');
 const cTable = require('console.table');
+const Departments = require('./lib/Departments');
+
+
 
 const nav_menu = [
     {
@@ -54,7 +57,7 @@ const emp_prompts = [
     {
         name: 'role',
         type: 'list',
-        message: 'Employee role:', 
+        message: 'Employee role:',
         choices: ['Manager', 'Supervisor', 'Comm Lead', 'Salesperson']
     },
     {
@@ -87,7 +90,9 @@ function selectedOption(choice) {
         db.query('SELECT * FROM employee', (err, data) => {
             if (err) return console.log(err);
 
-            console.table(data);
+            const emp_Table = cTable.getTable(data);
+
+            console.table(emp_Table)
             backToMenu();
         });
     };
@@ -97,17 +102,18 @@ function selectedOption(choice) {
         db.query('SELECT * FROM department', (err, data) => {
             if (err) return console.table(err);
 
-            console.table(data);
+            const dep_Table = cTable.getTable(data)
+            console.table(dep_Table);
             backToMenu();
         })
     };
 
     if (choice.nav === 'View All Roles') {
-
         db.query('SELECT * FROM roles', (err, data) => {
             if (err) return console.log(err);
 
-            console.table(data);
+            const role_Table = cTable.getTable(data)
+            console.table(role_Table);
             backToMenu();
 
         });
@@ -138,30 +144,45 @@ function newDepartment() {
 };
 
 function newRole() {
-    let departmentId;
+
+    db.query('SELECT * FROM department', (err, data) => {
+        for (let i = 0; i < data.length; i++) {
+            role_prompts[2].choices[i] = data[i].dep_name;
+        }
+    })
 
     inquirer.prompt(role_prompts)
         .then(answers => {
-            console.log(`Now adding ${answers.new_role}`);
+            console.log(`Now adding ${answers.new_role} to ${answers.department}`);
+            
 
-            if (answers.department === 'Sales') {
-                departmentId = 1;
-            } else if (answers.department === 'Communications') {
-                departmentId = 2;
-            } else if (answers.department === 'Management') {
-                departmentId = 3;
-            } else if (answers.department === 'Engineering') {
-                departmentId = 4;
-            };
+            let dep_id;
+            db.query('SELECT * FROM department', (err, data) => {
+                let chosenDepartment = answers.department;
 
-            db.query(`INSERT INTO roles (title, salary, department_id) VALUES ('${answers.new_role}', ${answers.salary}.000, ${departmentId})`)
-        }).then(() => {
+                for(let i = 0; i < data.length; i++) {
+                    // console.log(chosenDepartment, data[i].dep_name)
+                    if (data[i].dep_name == answers.department) {
+                        dep_id = data[i].id;
+                    } 
+                }
+
+                db.query(`INSERT INTO roles (title, salary, department_id) VALUES ('${answers.new_role}', ${answers.salary}, ${dep_id})`)
+            });
+        })
+        .then(() => {
             menu();
         })
 };
 
+
 // first_name, last_name, role, and manager
 function newEmployee() {
+    db.query('SELECT * FROM roles', (err, data) => {
+        for (let i = 0; i < data.length; i++) {
+            emp_prompts[2].choices[i] = data[i].title;
+        }
+    })
 
     inquirer.prompt(emp_prompts)
         .then(answers => {
@@ -173,7 +194,7 @@ function newEmployee() {
                 roleId = 2;
             } else if (answers.role === 'Comm Lead') {
                 roleId = 3;
-            } else if (answers.role === 'Sales'){
+            } else if (answers.role === 'Salesperson') {
                 roleId = 4;
             };
 
@@ -183,25 +204,4 @@ function newEmployee() {
         })
 }
 
-
-
-// .then((answer) => {
-// console.log(`Adding ${answer.new_dep}...`)
-// db.query(`INSERT INTO department (dep_name) VALUES ("${answer.new_dep}")`);
-// }).then(() => {
-
-// db.query('SELECT * FROM department', (err, data) => {
-//     if (err) return console.log(err);
-
-//     return console.table(data);
-// })
-// })
-
-
-
-// menu();
-
-
-// newDepartment();
-// welcome();
 menu()
